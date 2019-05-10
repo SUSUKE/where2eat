e555b9d4f6661dd6ad4322210c171cb7eae70181
SUSUKE <cesium321yo@gmail.com>
2019-05-10 15:57:00 +0900

    ５つからランダム表示

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,4 +1,7 @@
 class LunchController < ApplicationController
-      def go
+      def go #メソッドgoを定義
+      	  eataries = ['松のや', 'あすかて食堂', 'te.to.te', 'トタンコットンカフェ', '711で弁当'] #配列eatariesを作成し、文字列を格納する
+	  @recommend = eataries.sample #変数recomendに配列eatriesから一つとって返す（sampleメソッド）
+	  	       		       #@は他のところでも使えるようにするため
       end
 end
