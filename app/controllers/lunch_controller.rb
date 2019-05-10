875e7ce77acc887cc9140e871763af8cea0cc61f
SUSUKE <cesium321yo@gmail.com>
2019-05-10 17:13:30 +0900

    データベースを使用

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,6 +1,6 @@
 class LunchController < ApplicationController
       def go #メソッドgoを定義
-      	  eataries = ['松のや', 'あすかて食堂', 'te.to.te', 'トタンコットンカフェ', '711で弁当'] #配列eatariesを作成し、文字列を格納する
+      	  eataries = Eatery.all
 	  @recommend = eataries.sample #変数recomendに配列eatriesから一つとって返す（sampleメソッド）
 	  	       		       #@は他のところでも使えるようにするため
       end
---
+++ db/migrate/20190510073332_create_eateries.rb
@@ -0,0 +1,15 @@
+class CreateEateries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :eateries do |t|
+      t.string :name
+      t.string :zip
+      t.string :address
+      t.string :tel
+      t.string :reghol
+      t.string :open
+      t.string :close
+
+      t.timestamps
+    end
+  end
+end
