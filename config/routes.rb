c4406893418e145fcdb8506a3a9fadf9d0fc30e8
SUSUKE <cesium321yo@gmail.com>
2019-05-17 16:54:35 +0900

    routes.rbを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'eateries/new'
+  post 'eateries/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'lunch/go'#, to: 'luncj#go'
   #post 'eateries/create'
   root 'lunch#go'
-  post 'eateries/create'
+  #post 'eateries/create'
 end
