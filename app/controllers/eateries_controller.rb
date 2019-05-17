24adcb5fe1026df53d607c97727abbda3a10e0ed
SUSUKE <cesium321yo@gmail.com>
2019-05-17 17:03:31 +0900

    コントローラを修正

--- app/controllers/eateries_controller.rb
+++ app/controllers/eateries_controller.rb
@@ -5,7 +5,7 @@ class EateriesController < ApplicationController
   def create
       eatery = Eatery.new(eatery_params)
       eatery.save
-      flash[:success] = "#(eatery.name)を追加しました"
+      flash[:success] = "#{eatery.name}を追加しました"
       redirect_to root_path
   end
 
